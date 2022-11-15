9c5003b375a09e37856efe1a5c47055644285f98
Spencer Steffen <spencer@citrusme.com>
2022-11-15 14:52:43 -0800

    bind fastify server to 0.0.0.0

--- frontend/server.js
+++ frontend/server.js
@@ -1,11 +1,14 @@
-const fastify = require('fastify')({logger: true})
 const path = require('path')
+const fastify = require('fastify')({ logger: true })
 
 fastify.register(require('@fastify/static'), {
   root: path.join(__dirname, 'dist')
 })
 
-fastify.listen({ port: process.env.PORT || 3000 }, (err, address) => {
+fastify.listen({
+  host: '0.0.0.0',
+  port: process.env.PORT || 3000,
+}, (err, address) => {
   if (err) throw err
   // Server is now listening on ${address}
 })
