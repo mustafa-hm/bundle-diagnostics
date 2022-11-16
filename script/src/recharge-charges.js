97cb678abc60657d0bf6bff645ffc252378c9c20
Spencer Steffen <spencer@citrusme.com>
2022-11-16 11:27:08 -0800

    use latest recharge API version, add delays to prevent 504s

--- script/src/recharge-charges.js
+++ script/src/recharge-charges.js
@@ -2,32 +2,34 @@ import recharge from './recharge.js'
 import { addDays } from 'date-fns'
 import { verifyCustomer } from './customer.js'
 import { createIssues } from './issues.js'
+import { timer } from './utils.js'
 
 export default async function () {
   const twoDaysOut = addDays(new Date(), 2)
   let page = 1
   const fetchCharges = async (page) => {
-    const { data } = await recharge.get(`/charges?status=queued,pending&sort_by=id-asc&scheduled_at_max=${twoDaysOut.toJSON()}&page=${page}`)
+    const { data } = await recharge.get(`/charges?status=queued,pending&sort_by=scheduled_at-asc&scheduled_at_max=${twoDaysOut.toJSON()}&page=${page}`)
     return data.charges
   }
   let charges = await fetchCharges(page)
   while (charges?.length) {
     for (const charge of charges) {
-      const { customer_id, id: charge_id, scheduled_at } = charge
-      const issues = await verifyCustomer(customer_id)
+      const { customer, id: charge_id, scheduled_at } = charge
+      const issues = await verifyCustomer(customer.id)
       if (parseInt(charge.total_price) === 0) {
         issues.push({
           type: 'Free Charge'
         })
       }
-
       await createIssues({
-        customer_id,
+        customer_id: customer.id,
         charge_id,
         scheduled_at,
         issues
       })
+      await timer(250)
     }
+    await timer(500)
     page += 1
     charges = await fetchCharges(page)
   }
--- script/src/recharge.js
+++ script/src/recharge.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 export const recharge = axios.create({
   baseURL: 'https://api.rechargeapps.com/',
   headers: {
-    'X-Recharge-Access-Token': process.env.RECHARGE_API_TOKEN
+    'X-Recharge-Access-Token': process.env.RECHARGE_API_TOKEN,
+    'X-Recharge-Version': '2021-11'
   }
 })
 
