6f473aa777ffd4324276e49f5e97370897648077
Spencer Steffen <spencer@citrusme.com>
2022-11-15 16:03:30 -0800

    add auth to server

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,36 @@
 const path = require('path')
 const fastify = require('fastify')({ logger: true })
 
+const {
+  PORT,
+  USERNAME,
+  PASSWORD
+} = process.env
+
 fastify.register(require('@fastify/static'), {
   root: path.join(__dirname, 'dist')
 })
 
+if (USERNAME && PASSWORD) {
+  fastify.register(require('@fastify/basic-auth'), {
+    authenticate: {
+      realm: 'Application'
+    },
+    validate: async (username, password) => {
+      if (username !== USERNAME || password !== PASSWORD) {
+        return new Error('Authentication Failed')
+      }
+    }
+  })
+  fastify.after(() => {
+    fastify.addHook('onRequest', fastify.basicAuth)
+  })
+}
+
 fastify.listen({
   host: '0.0.0.0',
-  port: process.env.PORT || 3000,
-}, (err, address) => {
+  port: PORT || 3000,
+}, (err) => {
   if (err) throw err
   // Server is now listening on ${address}
 })
